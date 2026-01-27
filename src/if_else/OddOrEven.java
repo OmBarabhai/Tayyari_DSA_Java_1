12bb279f9637299446109c3608316e1bf20526cc
OmBarabhai <oraghuwanshi37@gmail.com>
2026-01-27 16:01:22 +0530

    Java Basics

---
+++ src/if_else/OddOrEven.java
@@ -0,0 +1,18 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int x = sc.nextInt();
+
+        if(x%2==0){
+            System.out.println("Number is Even: "+ x);
+        }
+        else{
+            System.out.println("Number is Odd: "+ x);
+        }
+    }
+}
