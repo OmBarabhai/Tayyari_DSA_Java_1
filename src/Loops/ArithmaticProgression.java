38448df41f24c7ec6d7323982eab2b39ae34bdbd
OmBarabhai <oraghuwanshi37@gmail.com>
2026-01-28 20:00:06 +0530

    Java Basics

---
+++ src/Loops/ArithmaticProgression.java
@@ -0,0 +1,21 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class ArithmaticProgression {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Number: ");
+
+        int n = sc.nextInt();
+//        for (int i = 2; i <= 2+(n-1)*3; i+=3) {
+//            System.out.print(i+ " ");
+//
+//
+//        }
+        for (int i = 2; i <= 3*n-1; i+=3) {
+            System.out.print(i+" ");
+
+        }
+    }
+}
