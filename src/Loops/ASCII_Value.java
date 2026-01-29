a3e9b9c58c22b6bc21ab7f6981a887f01624bdc2
OmBarabhai <oraghuwanshi37@gmail.com>
2026-01-29 10:45:35 +0530

    Java Basics

---
+++ src/Loops/ASCII_Value.java
@@ -0,0 +1,17 @@
+package Loops;
+
+
+public class ASCII_Value {
+    public static void main(String[] args) {
+
+        for (int i = 65 ,j=97; i <= 90; i++) {
+
+
+            System.out.println((char) i+"  "+ i);
+            System.out.println((char) j+"  "+ j);
+            j++;
+            System.out.println();
+
+        }
+    }
+}
