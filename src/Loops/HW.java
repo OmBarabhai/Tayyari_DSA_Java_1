0ccc3c3f1cac6426609bb5c94f1aa1d62b625daf
OmBarabhai <oraghuwanshi37@gmail.com>
2026-01-29 10:17:33 +0530

    Java Basics

---
+++ src/Loops/HW.java
@@ -0,0 +1,20 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class HW {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n= sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(i+" ");
+
+            System.out.print(n+" ");
+            n--;
+
+
+        }
+
+    }
+}
