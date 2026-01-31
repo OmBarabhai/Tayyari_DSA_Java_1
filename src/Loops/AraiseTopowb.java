79ef72b07cecbd13648be592db98f03ed8de5671
OmBarabhai <oraghuwanshi37@gmail.com>
2026-01-31 21:29:37 +0530

    Java Basics

---
+++ src/Loops/AraiseTopowb.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class AraiseTopowb {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("find a^b");
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter b: ");
+
+        int b = sc.nextInt();
+
+        int pow = 1;
+        for(int i =1; i<=b;i++){
+            pow *=a;
+        }
+        System.out.println("a^b: "+pow);
+
+
+    }
+}
