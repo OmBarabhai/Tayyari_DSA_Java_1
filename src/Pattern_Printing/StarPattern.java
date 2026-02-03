a2db38be659eca1e13337796a59d3380a247d916
OmBarabhai <oraghuwanshi37@gmail.com>
2026-02-03 22:45:03 +0530

    Java Basics

--- src/Pattern_Printing/StarPattern.java
+++ src/Pattern_Printing/StarPattern.java
@@ -6,6 +6,7 @@ public class StarPattern {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
 
+
         int row = sc.nextInt();
         int col = sc.nextInt();
 
