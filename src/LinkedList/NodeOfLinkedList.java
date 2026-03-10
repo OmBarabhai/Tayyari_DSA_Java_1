da9b379f6231c3c2499f5d16cddff4104877b7c2
OmBarabhai <oraghuwanshi37@gmail.com>
2026-03-10 22:34:55 +0530

    LinkedList

---
+++ src/LinkedList/NodeOfLinkedList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+class Node{
+    int val;
+    Node next; // null
+    Node(int val){
+        this.val = val;
+    }
+}
+public class NodeOfLinkedList {
+    public static void main(String[] args) {
+        // 10 -> 20 -> 30 -> 40 -> 50 -> null;
+
+        Node a = new Node(10);  //a.val = 10; LinkedList.Node@5f184fc6
+        Node b = new Node(20);  //b.val = 20;
+        Node c = new Node(30);  //c.val = 30;
+        Node d = new Node(40);  //d.val = 40;
+        Node e = new Node(50);  //e.val = 50;
+
+        //connect karenge (Link Karenge)
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(b.next);
+        System.out.println(a.next.next.val);
+
+
+    }
+}
