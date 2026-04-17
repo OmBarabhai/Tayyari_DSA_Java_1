12afe894e7cea09ac44c9717df1470bdefd13bb0
OmBarabhai <om.r.barabhai@gmail.com>
2026-04-17 23:35:21 +0530

    add Problem

---
+++ src/Queues/ReverseQueue.java
@@ -0,0 +1,14 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+
+    }
+    public void reverseQueue(Queue<Integer> q) {
+
+    }
+}
